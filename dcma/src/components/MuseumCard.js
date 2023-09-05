8f79f840226cfd896c12f33384d068397aa3dc33
Kim Love <kimberlymlove15@gmail.com>
2023-09-05 09:58:31 -0400

    Working navigation to specific museum

--- dcma/src/components/MuseumCard.js
+++ dcma/src/components/MuseumCard.js
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom"
 
 
 function MuseumCard({museum}){
-    const {name} = museum
+    const {name, id} = museum
     return(
-        <><p>{name}</p></>
+        <>
+            <p>{name}</p>
+            <Link to={`/MuseumList/${id}`}>See More...</Link>
+        </>
     )
 }
 
--- dcma/src/components/SpecificMuseum.js
+++ dcma/src/components/SpecificMuseum.js
@@ -1,14 +1,45 @@
+import { useEffect, useState } from "react";
 import CommentCard from "./CommentCard";
 import CommentForm from "./CommentForm";
+import { useParams } from "react-router-dom";
 
-function SpecificMuseum({ museum }) {
-  if (museum === undefined) {
+function SpecificMuseum({ museum, API }) {
+  // if (museum === undefined) {
+  //   return <h1>LOADING...</h1>;
+  // } else {
+  //   const {name,hours,desc,admission,address,image,rating,comment} = museum
+  //   return (
+  //     <>
+  //       <p>Inside else</p>
+  //       <div>
+  //         <h1>{name}</h1>
+  //         <img src={image} alt={name} />
+  //         {admission === 0 ? "test" : "test2"}
+  //       </div>
+  //       <div>
+  //         <p>{desc}</p>
+  //       </div>
+  //     </>
+  //   );
+  // }
+
+  const [specificMuseum, setSpecificMuseum] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`${API}/${id}`)
+    .then((res) => res.json())
+    .then((data) => setSpecificMuseum(data));
+  }, [id])
+
+  console.log(specificMuseum)
+  
+    if (specificMuseum === null) {
     return <h1>LOADING...</h1>;
   } else {
-    const {name,hours,desc,admission,address,image,rating,comment} = museum
+    const {name, admission, comment, desc, hours, image, rating} = specificMuseum
     return (
       <>
-        <p>Inside else</p>
         <div>
           <h1>{name}</h1>
           <img src={image} alt={name} />
@@ -20,6 +51,7 @@ function SpecificMuseum({ museum }) {
       </>
     );
   }
+
 }
 
 export default SpecificMuseum;
