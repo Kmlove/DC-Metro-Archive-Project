f16f699fd11b1bc097f04564ea11b705942e95be
Yurick <YJakse789@gmail.com>
2023-09-06 11:38:13 -0400

    Made Museum Card pretty, added css functionality

--- dcma/src/components/MuseumCard.js
+++ dcma/src/components/MuseumCard.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 
 function MuseumCard({museum, API, onRemoveMuseum}){
-    const {name, id} = museum
+    const {name, id, feedback} = museum
     
     function handleClick(){
         fetch(`${API}/${id}`, {
@@ -11,14 +11,28 @@ function MuseumCard({museum, API, onRemoveMuseum}){
         .then(res=> res.json())
         .then(data => onRemoveMuseum(museum))
     }
-
+    const arrayOfRating = feedback.map((Obj) => Obj.rating);
+    const averageRating = arrayOfRating.reduce((partSum, a) => partSum + a, 0)/arrayOfRating.length;
     return(
-        <>
+        <div style={CardStyle} className="MuseumCard">
             <p>{name}</p>
+            <p>Average Rating:{" "}
+            {isNaN(averageRating)
+              ? "No ratings yet"
+              : averageRating}</p>
             <Link to={`/museums/${id}`}>See More...</Link>
             <button onClick={handleClick}>Delete</button>
-        </>
+        </div>
     )
 }
+
+const CardStyle = {
+    width: "25%",
+    backgroundColor: "#f0f0f0",
+    padding: "20px",
+    borderRadius: "10px",
+    boxShadow: "0px 5px 15px rgba(0, 0, 0, 0.2)",
+    marginBottom: "20px",
+  };
 
 export default MuseumCard
--- dcma/src/components/MuseumList.js
+++ dcma/src/components/MuseumList.js
@@ -4,8 +4,18 @@ function MuseumList({museums, API, onRemoveMuseum}){
     console.log(museums)
     const renderMuseums = museums.map((museum)=>{return <MuseumCard key={museum.id} onRemoveMuseum={onRemoveMuseum} API={API} museum={museum}/>})
     return(
-        <>{renderMuseums}</>
+        <div style={ListStyle}>{renderMuseums}</div>
     )
 }
+
+const ListStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "2% 2%",
+    justifyContent: "center",
+    marginTop: "4%",
+    marginBottom: "4%",
+}
+
 
 export default MuseumList
