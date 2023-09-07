aeba6875599d326022a675404198c4c4832fbb90
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-09-07 15:17:34 -0400

    updates default image in card, too

--- dcma/src/components/MuseumCard.js
+++ dcma/src/components/MuseumCard.js
@@ -5,7 +5,7 @@ function MuseumCard({ museum, API, onRemoveMuseum, museums }) {
 
   if (image === "") {
     image =
-      "https://w7.pngwing.com/pngs/169/68/png-transparent-architectural-engineering-graphy-work-in-progress-angle-building-text.png";
+      "https://images.unsplash.com/photo-1481277542470-605612bd2d61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2006&q=80";
   }
 
   function handleClick() {
